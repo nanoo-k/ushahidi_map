609b4f4a3a467b4833ca7deda55e45bd950a78d9
Miguel Valencia <miguel.s.valencia@gmail.com>
2017-04-01 23:42:30 -0400

    Show info that reflects the existence of data with undefined values.

--- app/js/setup-map.js
+++ app/js/setup-map.js
@@ -60,16 +60,31 @@ map.on('move', onmove);
 onmove();
 
 
-var project_density = [];
+// CREATE project_density ARRAY
+var project_density_per_county = [];
 data.features.forEach(function(feature, i) {
-    var county = feature.properties.county;
-    if (project_density[county] == undefined) {
-        project_density[county] = 1;
-    } else {
-        project_density[county] = project_density[county] + 1;
+
+    // If the county property is null, skip
+    if (feature.properties.county == null) return;
+
+    var county = feature.properties.county.toUpperCase();
+
+    // Create the county property in the array if it doesn't exist
+    if (project_density_per_county[county] == undefined) {
+        project_density_per_county[county] = {
+            "number_of_projects": 1,
+            "projects_missing_from_map": 0
+        };
+    }
+    // Else increment the tally for the county
+    else {
+        ++project_density_per_county[county]["number_of_projects"];
+    }
+
+    if (feature.properties.x == null || feature.properties.y == null) {
+        ++project_density_per_county[county]["projects_missing_from_map"];
     }
 });
-
 
 //
 // CHOROPLETH
@@ -99,16 +114,29 @@ function getStyle(feature) {
 // get color depending on population density value
 function getColor(county) {
 
-    var COUNTY = project_density[county.toUpperCase()];
 
-    return COUNTY > 1000 ? '#8c2d04' :
-        COUNTY > 500  ? '#cc4c02' :
-        COUNTY > 200  ? '#ec7014' :
-        COUNTY > 100  ? '#fe9929' :
-        COUNTY > 50   ? '#fec44f' :
-        COUNTY > 20   ? '#fee391' :
-        COUNTY > 10   ? '#fff7bc' :
-        '#ffffe5';
+    // Some of the data arrives as something other than a string,
+    // thus has no access to toUpperCase()
+    if (typeof(county) === "string") {
+        var COUNTY = project_density_per_county[county.toUpperCase()];
+    }
+
+    if (COUNTY != null){
+        var number_of_projects = COUNTY["number_of_projects"];
+
+        return number_of_projects > 500 ? '#8c2d04' :
+            number_of_projects > 200  ? '#cc4c02' :
+            number_of_projects > 100  ? '#ec7014' :
+            number_of_projects > 50  ? '#fe9929' :
+            number_of_projects > 20   ? '#fec44f' :
+            number_of_projects > 10   ? '#fee391' :
+            number_of_projects > 5   ? '#fff7bc' :
+            '#ffffe5';
+    }
+
+
+    // If we have no data about this county, color it gray
+    return '#d1d1d1';
 }
 
 // Set event callbacks
@@ -128,8 +156,19 @@ function mousemove(e) {
 
     // Set position and content of the popup
     popup.setLatLng(e.latlng);
-    popup.setContent('<div class="marker-title">' + layer.feature.properties.COUNTY + '</div>' +
-        layer.feature.properties.PERIMETER + ' is PERIMETER number');
+
+    var content = '<div class="marker-title">' + layer.feature.properties.COUNTY + '</div>';
+
+    var county_data = project_density_per_county[layer.feature.properties.COUNTY.toUpperCase()];
+
+    if (county_data !== undefined) {
+        content += '<div>' + project_density_per_county[layer.feature.properties.COUNTY.toUpperCase()]["number_of_projects"] + ' is number of projects. </div>';
+
+        content += '<div>' + project_density_per_county[layer.feature.properties.COUNTY.toUpperCase()]["projects_missing_from_map"] + ' is number of projects missing from map. </div>';
+    }
+
+
+    popup.setContent(content);
 
     // Open the popup
     if (!popup._map) popup.openOn(map);
