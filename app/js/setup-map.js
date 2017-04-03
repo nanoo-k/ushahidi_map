f69d50673db0ecabf203bb7bc535d1af83ffae19
Miguel Valencia <miguel.s.valencia@gmail.com>
2017-04-02 20:54:54 -0400

    Explain what this variable is used for.

--- app/js/setup-map.js
+++ app/js/setup-map.js
@@ -14,6 +14,7 @@ var MyMap = {
 
     // Mapxbox cluster markers
     clusteredMarkers: new L.MarkerClusterGroup(),
+    // "Individual" markers are unclustered markers
     individualMarkers: [],
 
     // Collection for filtering only the "expensive" projects
